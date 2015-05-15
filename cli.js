1cce0ce855edb50e32a72db0f649beca3ba8942d
Eugene Sharygin <eush77@gmail.com>
2015-05-15 16:23:38 +0300

    (cli) Respond to --help

--- cli.js
+++ cli.js
@@ -4,6 +4,12 @@
 var fs = require('fs');
 
 
+if (process.argv.slice(2) == '--help') {
+  console.log('Usage:  wordhi <HTML');
+  process.exit();
+}
+
+
 process.stdin.pipe(process.stdout);
 
 process.stdin.on('end', function () {
