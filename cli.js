845708002403ea7a1c7bc2a1472669ff3f510a4f
Eugene Sharygin <eush77@gmail.com>
2015-05-29 11:46:33 +0300

    Move range-searching code to range-lookup

--- cli.js
+++ cli.js
@@ -1,19 +1,31 @@
 #!/usr/bin/env node
 'use strict';
 
-var fs = require('fs');
+var browserify = require('browserify'),
+    stread = require('stread');
 
 
 if (process.argv.slice(2) == '--help') {
-  console.log('Usage:  wordhi <HTML');
+  console.log(['Usage:  hise',
+               '',
+               'Reads stdin, writes to stdout.'].join('\n'));
   process.exit();
 }
+
+
+var bundle = function (cb) {
+  browserify(stread('require("./")()'), { basedir: __dirname })
+    .bundle(cb);
+};
 
 
 process.stdin.pipe(process.stdout);
 
 process.stdin.on('end', function () {
-  process.stdout.write('<script>' +
-                       fs.readFileSync(__dirname + '/index.js') +
-                       '</script>');
+  process.stdout.write('<script>');
+  bundle(function (err, buf) {
+    if (err) throw err;
+    process.stdout.write(buf.toString());
+    process.stdout.write('</script>');
+  });
 });
--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-document.addEventListener('mouseup', function () {
-  var selection = window.getSelection();
-  if (selection.isCollapsed || selection.rangeCount != 1) {
-    return;
-  }
-  var query = selection.toString();
-  if (query.indexOf('"') >= 0) {
-    console.error('word-highlight: Resist to search for query with quotes.');
-    return;
-  }
-  var xpathResult = document.evaluate('//text()[contains(.,"' + query + '")]', document, null, XPathResult.ANY_TYPE, null);
-  var node;
-  while (node = xpathResult.iterateNext()) {
-    var textContent = node.textContent;
-    var re = RegExp(query, 'g');
-    while (pos = re.exec(textContent)) {
-      var range = document.createRange();
-      range.setStart(node, pos.index);
-      range.setEnd(node, pos.index + query.length);
-      selection.addRange(range);
+'use strict';
+
+var rangeLookup = require('range-lookup');
+
+
+module.exports = function () {
+  document.addEventListener('mouseup', function () {
+    var selection = window.getSelection();
+    if (selection.isCollapsed || selection.rangeCount != 1) {
+      return;
     }
-  }
-});
+    var query = selection.toString();
+    rangeLookup(selection.toString())
+      .forEach(selection.addRange.bind(selection));
+  });
+};
