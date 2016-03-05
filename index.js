9a0047f6a314e200336d19fdea3e40846c375ef9
Eugene Sharygin <eush77@gmail.com>
2016-03-05 03:48:09 +0300

    inject scripts into HTML

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var browserify = require('browserify'),
+    scriptInjector = require('script-injector'),
     stread = require('stread'),
     through = require('through2'),
     duplexer = require('duplexer2'),
@@ -21,6 +22,16 @@ module.exports = function (opts) {
 
   opts = opts || {};
 
+  bundle({ ignoreCase: opts['ignore-case'] }, function (err, buf) {
+    if (err) throw err;
+
+    input
+      .pipe(scriptInjector(buf.toString()))
+      .pipe(output);
+  });
+
+  return duplexer(input, output);
+
   // TODO: This is broken in the sense that it doesn't work for HTML anymore.
   // What a shame.
   output.write('<pre>');
@@ -34,11 +45,6 @@ module.exports = function (opts) {
 
   input.on('end', function () {
     output.write('</pre><script>');
-    bundle({ ignoreCase: opts['ignore-case'] }, function (err, buf) {
-      if (err) throw err;
-      output.write(buf.toString());
-      output.end('</script>');
-    });
   });
 
   return duplexer(input, output);
--- test/test.js
+++ test/test.js
@@ -5,17 +5,21 @@ var hise = require('..');
 var tape = require('tape'),
     test = require('tape-drain')(tape),
     glob = require('glob'),
-    hyperstream = require('hyperstream');
+    hyperstream = require('hyperstream'),
+    trumpet = require('trumpet'),
+    through = require('through2');
 
 var basename = require('path').basename,
     fs = require('fs');
 
 
 test(function (t) {
+  var mainScript = fs.readFileSync(__dirname + '/../lib/script.js', 'utf8');
+
   glob(__dirname + '/fixtures/*([^.]).html', function (err, files) {
     if (err) throw err;
 
-    t.plan(files.length);
+    t.plan(2 * files.length);
 
     files.forEach(function (inputPath) {
       if (basename(inputPath).split('.').slice(1) != 'html') {
@@ -25,13 +29,21 @@ test(function (t) {
       var outputPath = inputPath.replace(/\.html$/, '.out.html');
       var name = basename(inputPath).split('.', 1)[0];
 
-      var output = fs.createReadStream(inputPath)
-            .pipe(hise())
-            .pipe(hyperstream({
-              'script': ''
-            }));
+      var output = fs.createReadStream(inputPath).pipe(hise());
 
-      t.drain.equal(output, fs.readFileSync(outputPath, 'utf8').trim(), name);
+      // Check HTML output.
+      t.drain.equal(output.pipe(hyperstream({ script: '' })),
+                    fs.createReadStream(outputPath),
+                    name);
+
+      // Check <script> contents.
+      output.pipe(trumpet()).createReadStream('script')
+        .pipe(through(function (chunk, enc, done) {
+          if (chunk.toString().indexOf(mainScript) >= 0) {
+            t.pass('script found');
+          }
+          done();
+        }));
     });
   });
 });
